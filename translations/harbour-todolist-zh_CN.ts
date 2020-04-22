02c5277a728ddbe2e55ab9ffdbac329fa6c7f504
Mirian Margiani <mirian@margiani.ch>
2020-04-22 18:59:47 +0200

    Add (fixed) zh_CN translation by dashinfantry

---
+++ translations/harbour-todolist-zh_CN.ts
@@ -0,0 +1,640 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About %1</source>
+        <translation>关于 %1</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>版本 %1</translation>
+    </message>
+    <message>
+        <source>Development</source>
+        <translation>-开发</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>作者</translation>
+    </message>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+    <message>
+        <source>&lt;i&gt;show contributors &lt;/i&gt;</source>
+        <translation>&gt;&lt;i&gt;显示贡献者 &lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;i&gt;show license &lt;/i&gt;</source>
+        <translation>&lt;i&gt;显示许可协议 &lt;/i&gt;</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation>数据</translation>
+    </message>
+    <message>
+        <source>Website</source>
+        <translation>网站</translation>
+    </message>
+    <message>
+        <source>Source Code</source>
+        <translation>源代码</translation>
+    </message>
+</context>
+<context>
+    <name>AddItemDialog</name>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Enter title</source>
+        <translation>输入标题</translation>
+    </message>
+    <message>
+        <source>Title</source>
+        <translation>标题</translation>
+    </message>
+    <message>
+        <source>Enter optional description</source>
+        <translation>输入可选描述</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Add an entry</source>
+        <translation>添加一则事项</translation>
+    </message>
+    <message>
+        <source>Scheduled for</source>
+        <translation>计划日期</translation>
+    </message>
+    <message>
+        <source>today</source>
+        <translation>今日</translation>
+    </message>
+    <message>
+        <source>tomorrow</source>
+        <translation>明日</translation>
+    </message>
+    <message>
+        <source>this week</source>
+        <translation>本周</translation>
+    </message>
+    <message>
+        <source>someday</source>
+        <translation>某日</translation>
+    </message>
+    <message>
+        <source>Project</source>
+        <translation>项目</translation>
+    </message>
+</context>
+<context>
+    <name>AddRecurringDialog</name>
+    <message>
+        <source>Add a recurring entry</source>
+        <translation>添加一则重复事项</translation>
+    </message>
+</context>
+<context>
+    <name>ArchivePage</name>
+    <message>
+        <source>Archived Entries</source>
+        <translation>已保存事项</translation>
+    </message>
+    <message>
+        <source>No entries yet</source>
+        <translation>暂无事项</translation>
+    </message>
+    <message>
+        <source>This page will show a list of all old entries.</source>
+        <translation>该页会显示所有旧事项列表。</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmDeleteDialog</name>
+    <message>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+    <message>
+        <source>Text</source>
+        <translation>文本</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>ContributorsPage</name>
+    <message>
+        <source>Contributors</source>
+        <translation>贡献</translation>
+    </message>
+    <message>
+        <source>Development</source>
+        <translation>开发</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>翻译</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Todo List</source>
+        <translation>待办列表</translation>
+    </message>
+</context>
+<context>
+    <name>EditItemDialog</name>
+    <message>
+        <source>Edit entry</source>
+        <translation>编辑事项</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+</context>
+<context>
+    <name>IntervalCombo</name>
+    <message>
+        <source>Recurring</source>
+        <translation>重复</translation>
+    </message>
+    <message>
+        <source>once</source>
+        <comment>interval for recurring entries</comment>
+        <translation>立即</translation>
+    </message>
+    <message numerus="yes">
+        <source>every %n day(s)</source>
+        <comment>interval for recurring entries</comment>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+</context>
+<context>
+    <name>ProjectsView</name>
+    <message>
+        <source>Projects</source>
+        <translation>项目</translation>
+    </message>
+    <message>
+        <source>Add project</source>
+        <translation>添加项目</translation>
+    </message>
+    <message>
+        <source>All entries belonging to this project will be deleted!</source>
+        <translation>全部属于该项目的旧事项将会被删除！</translation>
+    </message>
+    <message>
+        <source>mark as active</source>
+        <translation>标记为活动</translation>
+    </message>
+    <message>
+        <source>mark as halted</source>
+        <translation>标记为已停止</translation>
+    </message>
+    <message>
+        <source>mark as finished</source>
+        <translation>标记为已完成</translation>
+    </message>
+    <message>
+        <source>edit or delete</source>
+        <translation>编辑或删除</translation>
+    </message>
+    <message>
+        <source>edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <source>No entries</source>
+        <translation>暂无事项</translation>
+    </message>
+    <message>
+        <source>This should not be possible. Most probably a database error occurred.</source>
+        <translation>无法完成，可能发生了数据库错误。</translation>
+    </message>
+    <message>
+        <source>Add a project</source>
+        <translation>添加一个项目</translation>
+    </message>
+    <message>
+        <source>Edit project</source>
+        <translation>编辑项目</translation>
+    </message>
+</context>
+<context>
+    <name>RecurringsView</name>
+    <message>
+        <source>Recurring Entries</source>
+        <translation>重复事项</translation>
+    </message>
+    <message>
+        <source>Add recurring entry</source>
+        <translation>添加重复事项</translation>
+    </message>
+    <message>
+        <source>This will &lt;i&gt;not&lt;/i&gt; delete entries retroactively.</source>
+        <translation>删除事项的操作不可逆。</translation>
+    </message>
+    <message>
+        <source>mark as active</source>
+        <translation>标记为活动</translation>
+    </message>
+    <message>
+        <source>mark as halted</source>
+        <translation>标记为</translation>
+    </message>
+    <message>
+        <source>mark as done</source>
+        <translation>标记为完成</translation>
+    </message>
+    <message>
+        <source>No entries yet</source>
+        <translation>暂无事项</translation>
+    </message>
+    <message>
+        <source>This page will show a list of all recurring entries.</source>
+        <translation>该页会显示所有重复事项。</translation>
+    </message>
+    <message>
+        <source>Edit recurring entry</source>
+        <translation>编辑重复事项</translation>
+    </message>
+    <message>
+        <source>press and hold to edit or delete</source>
+        <translation>按下并握住以编辑或删除</translation>
+    </message>
+</context>
+<context>
+    <name>StartDateButton</name>
+    <message>
+        <source>Starting at</source>
+        <translation>开始时间</translation>
+    </message>
+</context>
+<context>
+    <name>TodoList</name>
+    <message>
+        <source>Today</source>
+        <translation>今日</translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation>明日</translation>
+    </message>
+    <message>
+        <source>No entries yet</source>
+        <translation>暂无事项</translation>
+    </message>
+    <message>
+        <source>Pull down to add entries</source>
+        <translation>下拉以添加事项</translation>
+    </message>
+    <message>
+        <source>Someday</source>
+        <translation>某天</translation>
+    </message>
+    <message>
+        <source>This week</source>
+        <translation>本周</translation>
+    </message>
+</context>
+<context>
+    <name>TodoListBaseItem</name>
+    <message>
+        <source>Edit entry</source>
+        <translation>编辑事项</translation>
+    </message>
+</context>
+<context>
+    <name>TodoListItem</name>
+    <message>
+        <source>continue today</source>
+        <translation>今日继续</translation>
+    </message>
+    <message>
+        <source>done</source>
+        <translation>完成</translation>
+    </message>
+    <message>
+        <source>done for today, continue tomorrow</source>
+        <translation>今日完成，明日继续</translation>
+    </message>
+    <message>
+        <source>ignore</source>
+        <translation>忽略</translation>
+    </message>
+    <message>
+        <source>continue tomorrow</source>
+        <translation>今日继续</translation>
+    </message>
+    <message>
+        <source>not completely done yet</source>
+        <translation>尚未完成</translation>
+    </message>
+    <message>
+        <source>⭑ %1, %2</source>
+        <translation>⭑ %1, %2</translation>
+    </message>
+    <message>
+        <source>from today</source>
+        <translation>从今日</translation>
+    </message>
+    <message>
+        <source>from yesterday</source>
+        <translation>从明日</translation>
+    </message>
+    <message>
+        <source>from earlier</source>
+        <translation>从更早日期</translation>
+    </message>
+    <message>
+        <source>for today</source>
+        <translation>今日</translation>
+    </message>
+    <message>
+        <source>carried over</source>
+        <translation>延期至</translation>
+    </message>
+    <message>
+        <source>ignored today</source>
+        <translation>今日忽略</translation>
+    </message>
+    <message>
+        <source>to be done tomorrow</source>
+        <translation>明日要完成</translation>
+    </message>
+    <message>
+        <source>done today</source>
+        <translation>今日完成</translation>
+    </message>
+    <message>
+        <source>press and hold to edit or delete</source>
+        <translation>按下并握住以编辑或删除</translation>
+    </message>
+    <message>
+        <source>move to someday later</source>
+        <translation>移动到稍后某日</translation>
+    </message>
+    <message>
+        <source>continue next day</source>
+        <translation>下一日继续</translation>
+    </message>
+    <message>
+        <source>from this day</source>
+        <translation>来自该日</translation>
+    </message>
+    <message>
+        <source>from last day</source>
+        <translation>来自上一日</translation>
+    </message>
+    <message>
+        <source>for this day</source>
+        <translation>该日</translation>
+    </message>
+    <message>
+        <source>for someday later</source>
+        <translation>稍后某日</translation>
+    </message>
+    <message>
+        <source>ignored this day</source>
+        <translation>该日忽略</translation>
+    </message>
+    <message>
+        <source>to be done next day</source>
+        <translation>下一日要完成</translation>
+    </message>
+    <message>
+        <source>to be done someday later</source>
+        <translation>稍后某日要完成</translation>
+    </message>
+    <message>
+        <source>done this day</source>
+        <translation>该日完成</translation>
+    </message>
+    <message>
+        <source>handle today</source>
+        <translation>今日处理</translation>
+    </message>
+    <message>
+        <source>continue later this week</source>
+        <translation>本周稍后继续</translation>
+    </message>
+    <message>
+        <source>handle later this week</source>
+        <translation>本周稍后处理</translation>
+    </message>
+    <message>
+        <source>to be done later this week</source>
+        <translation>本周稍后要完成</translation>
+    </message>
+    <message>
+        <source>continue someday later</source>
+        <translation>稍后某日继续</translation>
+    </message>
+</context>
+<context>
+    <name>TodoListView</name>
+    <message>
+        <source>Todo List</source>
+        <translation>待办列表</translation>
+    </message>
+    <message>
+        <source>Show old entries</source>
+        <translation>显示旧事项</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>笔记</translation>
+    </message>
+    <message>
+        <source>Swipe left to add recurring entries. You can specify an interval in which they will be added automatically to the current todo list.</source>
+        <translation>左滑以添加重复事项。你可以指定自动添加到当前待办列表中的时间间隔。</translation>
+    </message>
+    <message>
+        <source>Add entry</source>
+        <translation>添加事项</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <source>Programming</source>
+        <translation>编程</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation>英语</translation>
+    </message>
+    <message>
+        <source>German</source>
+        <translation>德语</translation>
+    </message>
+    <message>
+        <source>Todo List</source>
+        <translation>待办列表</translation>
+    </message>
+    <message>
+        <source>A simple tool for planning what to do next.</source>
+        <translation>一个简单的工具，用于规划接下来要做什么。</translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation>源代码位于 GiHub</translation>
+    </message>
+    <message>
+        <source>Icon Design</source>
+        <translation>图标设计</translation>
+    </message>
+    <message>
+        <source>Chinese</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>harbour-todolist</name>
+    <message>
+        <source>d MMM yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <translation>d MMM yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</translation>
+    </message>
+    <message>
+        <source>hh&apos;:&apos;mm</source>
+        <translation>hh&apos;:&apos;mm</translation>
+    </message>
+    <message>
+        <source>ddd d MMM yyyy</source>
+        <translation>ddd d MMM yyyy</translation>
+    </message>
+    <message>
+        <source>d MMM yyyy</source>
+        <translation>d MMM yyyy</translation>
+    </message>
+    <message>
+        <source>Database Error</source>
+        <translation>数据库错误</translation>
+    </message>
+    <message>
+        <source>Todo List</source>
+        <translation>待办列表</translation>
+    </message>
+</context>
+<context>
+    <name>storage</name>
+    <message>
+        <source>Default</source>
+        <translation>默认</translation>
+    </message>
+    <message>
+        <source>Failed to initialize database</source>
+        <translation>初始化数据库失败</translation>
+    </message>
+    <message>
+        <source>Empty database query</source>
+        <translation>数据库检索为空</translation>
+    </message>
+    <message>
+        <source>This is a programming error. Please file a bug report.</source>
+        <translation>这是一个编程错误。请以文件形式发送报告。</translation>
+    </message>
+    <message>
+        <source>Database access failed</source>
+        <translation>数据库访问失败</translation>
+    </message>
+    <message>
+        <source>Failed to update project</source>
+        <translation>更新项目失败</translation>
+    </message>
+    <message>
+        <source>No internal project ID was provided.</source>
+        <translation>没有提供内部项目ID。</translation>
+    </message>
+    <message>
+        <source>Failed to delete project</source>
+        <translation>删除项目失败</translation>
+    </message>
+    <message>
+        <source>The default project cannot be deleted.</source>
+        <translation>无法删除默认项目。</translation>
+    </message>
+    <message>
+        <source>Failed to update entry</source>
+        <translation>更新事项失败</translation>
+    </message>
+    <message>
+        <source>No internal entry ID was provided.</source>
+        <translation>没有提供内部事项ID。</translation>
+    </message>
+    <message>
+        <source>Failed to carry over old entries</source>
+        <translation>无法推迟旧事项</translation>
+    </message>
+    <message>
+        <source>Copying old entries failed.</source>
+        <translation>复制旧事项失败。</translation>
+    </message>
+    <message>
+        <source>Updating old entries failed.</source>
+        <translation>更新旧事项失败。</translation>
+    </message>
+    <message>
+        <source>Failed to update recurring entry</source>
+        <translation>更新重复事项失败</translation>
+    </message>
+    <message>
+        <source>Failed to delete recurring entry</source>
+        <translation>删除重复事项失败</translation>
+    </message>
+    <message>
+        <source>Failed to delete entry</source>
+        <translation>删除事项失败</translation>
+    </message>
+    <message>
+        <source>Failed to update recurring entries</source>
+        <translation>更新重复事项失败</translation>
+    </message>
+    <message>
+        <source>Copying new entries failed.</source>
+        <translation>复制新事项失败。</translation>
+    </message>
+    <message>
+        <source>Updating reference entries failed.</source>
+        <translation>更新相关事项失败。</translation>
+    </message>
+</context>
+</TS>
