a145e53b8c5a2a2e8d2ce38bae6f71a7b1592ad7
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2022-03-24 11:21:40 +0100

    Add a basic Sailjail profile (untested)

--- src/harbour-todolist.cpp
+++ src/harbour-todolist.cpp
@@ -1,42 +1,22 @@
 /*
  * This file is part of harbour-todolist.
- * SPDX-FileCopyrightText: 2020-2021 Mirian Margiani
+ * SPDX-FileCopyrightText: 2020-2022 Mirian Margiani
  * SPDX-License-Identifier: GPL-3.0-or-later
- *
- * harbour-todolist is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * harbour-todolist is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with harbour-todolist.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
+#ifdef QT_QML_DEBUG
 #include <QtQuick>
+#endif
+
 #include <sailfishapp.h>
 #include "requires_defines.h"
 
 int main(int argc, char *argv[])
 {
-    // SailfishApp::main() will display "qml/harbour-todolist.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
-
-    // return SailfishApp::main(argc, argv);
-
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
+    app->setOrganizationName("harbour-todolist"); // needed for Sailjail
+    app->setApplicationName("harbour-todolist");
+
     QScopedPointer<QQuickView> view(SailfishApp::createView());
     view->rootContext()->setContextProperty("APP_VERSION", QString(APP_VERSION));
     view->rootContext()->setContextProperty("APP_RELEASE", QString(APP_RELEASE));
