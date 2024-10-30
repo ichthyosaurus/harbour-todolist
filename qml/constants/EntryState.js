ce502dad98732a9c20a127071a728a9636a3230f
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-10-30 21:23:43 +0100

    Capitalize EntryState values

--- qml/constants/EntryState.js
+++ qml/constants/EntryState.js
@@ -1,21 +1,7 @@
 /*
  * This file is part of harbour-todolist.
- *
- * SPDX-FileCopyrightText: 2020 Mirian Margiani
- *
+ * SPDX-FileCopyrightText: 2020-2024 Mirian Margiani
  * SPDX-License-Identifier: GPL-3.0-or-later
- *
- * harbour-todolist is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * harbour-todolist is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 .pragma library
@@ -23,3 +9,7 @@
 var todo = 0;
 var ignored = 1;
 var done = 2;
+
+var Todo = 0
+var Ignored = 1
+var Done = 2
