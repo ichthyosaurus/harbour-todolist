8c09fdeb32e93db8b33854f54e76e10e75cc1639
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-11-05 14:37:38 +0100

    Add country to nn_NO metadata

--- translations/harbour-todolist-nn_NO.ts
+++ translations/harbour-todolist-nn_NO.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nn" sourcelanguage="en">
+<TS version="2.1" language="nn_NO" sourcelanguage="en_US">
 <context>
     <name>AboutPage</name>
     <message>
